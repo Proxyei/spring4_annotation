5f91e5f61aa58c0901574ef3c9f7e610d1a1808d
wodooWindows <insoul_net@qq.com>
2019-06-06 12:29:43 +0800

    删掉web.xml文件

--- src/test/java/spring4_annotation/Test_Configuration.java
+++ src/test/java/spring4_annotation/Test_Configuration.java
@@ -1,7 +1,10 @@
 package spring4_annotation;
 
+import java.util.Map;
+
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import com.xywei.spring4.configuration.AppConfiguration;
 import com.xywei.spring4.service.UserService;
@@ -23,7 +26,13 @@ public class Test_Configuration {
 		UserService userService = (UserService) context.getBean("userService001");
 //		UserService userService = (UserService) context.getBean(UserServiceImpl.class);
 		System.out.println("userService>>>>>>>>>>>>>>>>"+userService);
-
+		ConfigurableEnvironment environment = context.getEnvironment();
+		Map<String, Object> systemProperties = environment.getSystemProperties();
+		System.out.println(systemProperties);
+		String[] activeProfiles = environment.getActiveProfiles();
+		for (String string : activeProfiles) {
+			System.out.println(string);
+		}
 		context.close();
 	}
 
